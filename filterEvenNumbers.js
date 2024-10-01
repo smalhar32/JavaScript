a20cba7d57d64424f06f08788c34dcf892a4ee7f
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-01 10:19:44 +0530

    spelling mistake in function name

--- filterEvenNumbers.js
+++ filterEvenNumbers.js
@@ -1,8 +1,8 @@
-function filterEvemNumbers(numbers) {
+function filterEvenNumbers(numbers) {
     // Use the filter() method to keep only even numbers
     return numbers.filter(function(num){
         return num % 2 === 0;
     })
 }
-let evenNumbers = filterEvemNumbers([1,,2,3,4,5,6]);
+let evenNumbers = filterEveNumbers([1,,2,3,4,5,6]);
 console.log(evenNumbers);
