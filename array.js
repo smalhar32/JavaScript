d328921361ff7a38a419328b34d163823e33d122
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-02 09:44:32 +0530

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+const fruits = ['apple', 'banana','mango','orange']
+console.log(fruits);
+console.log(fruits[2]);
+fruits[2] = 'grapes'
+console.log(fruits);
+fruits.pop(length - 1)  // to delete last value
+console.log(fruits);
+fruits.push('watermelon')  // to add value at the end
+console.log(fruits);
+fruits[10] = 'papaya'
+console.log(fruits);
