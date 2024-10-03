216774a7d9bb87c242ed12a36f345a314abc9ee1
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-03 12:54:47 +0530

    reverse an array

---
+++ reverseArray.js
@@ -0,0 +1,22 @@
+//Reverse an array
+//1
+let reverseArray = (arr) => (arr.reverse())
+console.log(reverseArray([1, 2, 3, 4, 5])) //
+
+//2
+// function reverseArray(arr){
+//     return arr.reverse();
+// }
+// console.log(reverseArray([1,2,3,4,5])); 
+
+//3
+// function reverseArray(arr) {
+//     let reversedArray = []; //step 1: Initiallize an empty array
+//     //step 2:  Loop through the array from the end to the start
+//     for (let i = arr.length-1; i>=0; i--) {
+//         reversedArray.push(arr[i]); //step 3:  Add each element to the new array
+//     }
+//     return reversedArray;
+// }
+
+// console.log(reverseArray([1,2,3,4,5]));
