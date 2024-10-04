5229148edda0a339e77f666131183a878a1e9187
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-04 12:05:48 +0530

    updated

--- multi-dimentional array.js
+++ multi-dimentional array.js
@@ -1,18 +1,34 @@
 //multi-dimentional array is an array in which there are multiple arrays inside array 
 // Or you can say that array inside array
 
-// const nameNumber = [
-//   ["Adarsh", 75],
-//   ["Shima", 90],
-// ];
-// console.log(nameNumber);
-// console.log(nameNumber[1]);
+const fruits = ['Mango', 'Apple', 'Orange']
 
+//Using assign object
+// const myFruits=[] 
+// Object.assign(myFruits, fruits)
+
+//Using  spread operator(...)
+const myFruits = [...fruits]
+//Shallow copy
+myFruits.push('Dates')
+myFruits.push('Grapes')
+
+const user1={
+    firstName:'Anurag',
+    lastName: 'Singh'
+}
+
+// const  user2 = {}
+
+// Object.assign(user2,user1)
+
+const user2 = {...user1}
+user2.firstName = 'Rahul'
 //tic-tac-toe
 
-const ticTacToe = [
-  ['X',null,null],
-  [null,null,'O'],
-  ['O',null,'X'],
-];
-console.log(ticTacToe);
+// const ticTacToe = [
+//   ['X',null,null],
+//   [null,null,'O'],
+//   ['O',null,'X'],
+// ];
+// console.log(ticTacToe);
