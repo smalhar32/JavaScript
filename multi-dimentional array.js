cbe5f4fd2a23e3df5bb20db07ebe3f6acb144b1b
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-04 11:18:16 +0530

    multi dimentional array

---
+++ multi-dimentional array.js
@@ -0,0 +1,18 @@
+//multi-dimentional array is an array in which there are multiple arrays inside array 
+// Or you can say that array inside array
+
+// const nameNumber = [
+//   ["Adarsh", 75],
+//   ["Shima", 90],
+// ];
+// console.log(nameNumber);
+// console.log(nameNumber[1]);
+
+//tic-tac-toe
+
+const ticTacToe = [
+  ['X',null,null],
+  [null,null,'O'],
+  ['O',null,'X'],
+];
+console.log(ticTacToe);
