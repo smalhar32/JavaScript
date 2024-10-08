1a2748cc7c71ea8f3e587539ea5c3cd4f2e2f8e0
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-08 11:38:37 +0530

    function definition and function call

---
+++ function.js
@@ -0,0 +1,10 @@
+//function definition
+function greet(name, name2) {
+  console.log(`Hii ${name}`);
+  console.log("How are you?");
+  console.log(`My name is ${name2}`);
+}
+//function call
+greet('shivam','pooja');
+
+greet('Raj','john');
