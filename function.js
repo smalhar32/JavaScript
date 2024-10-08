0f3e0f1fc7f7e5cbdb5143170f92e557d73aa2cd
user.email like: "smalhar32@gmail.com <smalhar32@gmail.com>
2024-10-08 11:51:50 +0530

    sum of two numbers using function

--- function.js
+++ function.js
@@ -1,10 +1,16 @@
-//function definition
-function greet(name, name2) {
-  console.log(`Hii ${name}`);
-  console.log("How are you?");
-  console.log(`My name is ${name2}`);
-}
-//function call
-greet('shivam','pooja');
+// //function definition
+// function greet(name, name2) {
+//   console.log(`Hii ${name}`);
+//   console.log("How are you?");
+//   console.log(`My name is ${name2}`);
+// }
+// //function call
+// greet('shivam','pooja');
 
-greet('Raj','john');
+// greet('Raj','john');
+
+function sum(num1,num2) {
+    return num1+num2;
+}
+let result = sum(5,6);
+console.log(result);
